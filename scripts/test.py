ef7e6483eb994168140ecbbe5c3bd28bda5b10e3
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2022-02-11 17:15:27 -0800

    asdf

--- scripts/test.py
+++ scripts/test.py
@@ -11,16 +11,16 @@ import fcvision.run_utils as ru
 
 def main():
 	model = PlModel.load_from_checkpoint(params['checkpoint'], params=params, logdir=logdir).eval().cuda()
-    cfg, params = parse_yaml(osp.join("cfg", "test_config.yaml"))
+	cfg, params = parse_yaml(osp.join("cfg", "test_config.yaml"))
 
-    logdir = ru.get_file_prefix(params)
-    os.makedirs(os.path.join(logdir, 'lightning_logs'))
+	logdir = ru.get_file_prefix(params)
+	os.makedirs(os.path.join(logdir, 'lightning_logs'))
 
-    model = params["model"]
-    model.logdir = logdir
+	model = params["model"]
+	model.logdir = logdir
 
-    dataset_val = params['dataset_val']
-    for idx in range(len(dataset_val)):
+	dataset_val = params['dataset_val']
+	for idx in range(len(dataset_val)):
 		im = torch.unsqueeze(dataset_val[idx], 0)
 		with torch.no_grad():
 			pred = model(im)[0, 0].numpy()
