32637a0dd78e48ee0a9be25c2ea119abbce193ac
root <bthananjeyan@berkeley.edu>
2022-02-23 19:31:15 +0000

    testing

--- scripts/test.py
+++ scripts/test.py
@@ -2,6 +2,8 @@ import os
 import os.path as osp
 from PIL import Image
 import torch
+import numpy as np
+
 
 import fcvision.utils.run_utils as ru
 from fcvision.utils.arg_utils import parse_yaml
@@ -20,9 +22,17 @@ def main():
     for idx in range(len(dataset_val)):
         im = torch.unsqueeze(dataset_val[idx], 0).cuda()
         with torch.no_grad():
-            pred = model(im)[0, 0].cpu().numpy()
-        im = Image.fromarray(pred).convert("L")
-        im.save(osp.join(logdir, "vis", "pred_%d.jpg" % idx))
+            pred = torch.sigmoid(model(im))[0, 0].cpu().numpy()
+        im = np.transpose(im.cpu().numpy()[0], (1, 2, 0))
+        import IPython; IPython.embed(); assert 0
+        overlayed = np.copy(im)
+        overlayed[pred > 0.25] = [1, 0, 0]
+        im = Image.fromarray((im*255).astype(np.uint8))
+        im.save(osp.join(logdir, "vis", "%d_im.jpg" % idx))
+        pred = Image.fromarray((pred*255).astype(np.uint8)).convert("L")
+        pred.save(osp.join(logdir, "vis", "%d_pred.jpg" % idx))
+        overlayed = Image.fromarray((overlayed*255).astype(np.uint8))
+        overlayed.save(osp.join(logdir, "vis", "%d_overlayed.jpg" % idx))
 
 
 if __name__ == "__main__":
